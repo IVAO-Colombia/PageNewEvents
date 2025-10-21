7520b2a39da37094df6f3e707fa6b476b1b52ff2
Sosorioo <Osoriosairon244@gmail.com>
2025-10-20 19:13:54 -0500

    Añadir el recuento de noticias activas al widget de resumen de estadísticas
    Introduce una nueva estadística en el widget StatsOverview que muestra el recuento de noticias activas. Implementa el método getNewsActive para recuperar el recuento de noticias en las que «is_active» es verdadero.

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Event;
+use App\Models\News;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Support\Icons\Heroicon;
@@ -21,6 +22,10 @@ class StatsOverview extends StatsOverviewWidget
                 ->description('News Events registered')
                 ->descriptionIcon(Heroicon::Calendar)
                 ->color('success'),
+            Stat::make('Active News', $this->getNewsActive())
+                ->description('News Events active')
+                ->descriptionIcon(Heroicon::CheckCircle)
+                ->color('success'),
         ];
     }
 
@@ -32,5 +37,10 @@ class StatsOverview extends StatsOverviewWidget
     public function countEventRegister()
     {
         return Event::count();
+    }
+
+    public function getNewsActive()
+    {
+        return News::where('is_active', true)->count();
     }
 }
